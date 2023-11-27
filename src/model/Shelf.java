7deb389b5975f24724145302c12bf57391eb600e
Rasmus T <eaa23rbt@students.eaaa.dk>
2023-11-27 12:13:14 +0100

    updated Shelf class and the method in it addCask

--- src/model/Shelf.java
+++ src/model/Shelf.java
@@ -7,24 +7,21 @@ public class Shelf {
     private int number;
     private int rows;
     private int columns;
-    private ArrayList<Cask>[][] casks;
+    private List<Cask>[][] pallets;
 
     public Shelf(int number, int rows, int columns) {
         this.number = number;
         this.rows = rows;
         this.columns = columns;
-        this.casks = new ArrayList<Cask>[rows][columns];
+        this.pallets = new List[rows][columns];
     }
+
     public void addCask(Cask cask, int rowIn, int columnIn) {
         int row = rowIn - 1;
-        int column = columnIn -1;
+        int column = columnIn - 1;
         if (row >= 0 && row < rows && column >= 0 && column < columns) {
-            if (casks[row][column] == null) {
-                casks[row][column] = cask;
-                System.out.println("" + cask.getCaskNumber() + " added to row " + row + ", column " + column);
-            } else {
-                System.out.println("There is already a cask in this position");
-            }
+            pallets[row][column].add(cask);
+            System.out.println("" + cask.getCaskNumber() + " added to row " + row + ", column " + column);
         } else {
             System.out.println("Invalid position on shelf");
         }
