e17a4e6d04220d749a74984a35741de60ecc180b
Maksi333 <Simon_n_andersen@live.dk>
2023-11-28 12:21:19 +0100

    Setup

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,46 @@
 package controller;
 
+import model.*;
+
 public abstract class Controller {
+
+
+    //@Todo
+    public Cask createCask(){
+
+
+        return cask;
+    }
+
+    //Todo
+    public Warehouse createWarehouse(){
+
+        return warehouse;
+    }
+
+    //TODO
+    public Distillate createDistillate(){
+
+        return distillate;
+    }
+
+    //TODO
+    public Distillation createDistillation(){
+
+        return distillation;
+    }
+
+    //TODO
+    public Bottle createBottle(){
+
+        return bottle;
+    }
+
+    //TODO
+    public Cask fillCask(){
+
+        return cask;
+    }
+
+
 }
